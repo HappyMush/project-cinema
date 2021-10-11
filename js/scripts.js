280006a33eae3795596ccdd40e36c3b1f87f5a04
Vladislav Aleksandrov <happymushroom93@gmail.com>
2021-10-11 13:29:03 +0300

    Adaptive version. Fancybox for trailer and screenshots.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 const schemeSvg = document.querySelector('.scheme-svg');
-const totalPriceTag = document.querySelector('.price-total')
+const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 450;
 let totalPrice=0;
 schemeSvg.addEventListener('click', (event) => {
-    console.log(event.target);
+    /*console.log(event.target);*/
     if (!event.target.classList.contains('booked')&&!event.target.classList.contains('light'))
     {event.target.classList.toggle('active');
         let totalSeats = schemeSvg.querySelectorAll('.active').length;
@@ -11,3 +13,7 @@ schemeSvg.addEventListener('click', (event) => {
     totalPriceTag.textContent = totalPrice + " ";
         }
     });
+menuButton.addEventListener('click', () => {
+/*console.log('click done');*/
+menu.classList.toggle('is-open');
+})
